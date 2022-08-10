403ba2cde9b7e4a6956f49d0a0803dfedd3ce240
UnitedCatdom <kgoszczycki3636@gmail.com>
2022-08-10 15:47:20 +0200

    Improved URL input with cloud TLD checks

--- browser.py
+++ browser.py
@@ -1,7 +1,7 @@
 #!/bin/python3
 
 # libs, libs, libs
-import sys, os, json, urllib, platform, threading
+import sys, os, json, urllib, platform, threading, requests
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
@@ -29,6 +29,47 @@ HOMEPAGE_URL = settings.get("homepage")
 
 # and here search engine
 BROWSER_SE = settings.get("search_engine").get(settings.get("search_engine").get("current"))
+
+
+# all top-level domains 
+# also known as the two to three letters words after a dot
+# example: "https://www.google.com/"
+#                             ^^^^
+TLD_DOMAINS = []
+
+# get the domains from file 
+TLD_f = open("misc/tlds.txt", "r+")
+TLD_DOMAINS = TLD_f.read().split("\n") 
+TLD_f.close()
+
+
+# check for all top-level domains for intelligent url input 
+def __TLD_CLOUD_CHECK__():
+    # send request and download the content
+    _domains = requests.get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt").content.decode("ascii") 
+
+    domains = ""
+
+    # remove all comments and leave domains only
+    for tld in _domains.split("\n"):
+        if not tld.startswith("#"):
+            domains += f"{tld}\n"
+
+
+    # save it to file 
+    if not domains == "":
+        f = open("misc/tlds.txt", "w+") 
+        f.write(domains) 
+        f.close() 
+
+def check_for_tlds(): 
+    # tld check thread to eventually prevent from the whole app's crashing 
+    # and simulatenuously check the domains and run the app
+    threading.Thread(None, __TLD_CLOUD_CHECK__).start()
+
+check_for_tlds()
+
+
 
 
 # and the actual app
@@ -121,10 +162,16 @@ class App(QMainWindow):
             urltg = self.browser_urlbar.text()
 
         # smart but dumb url checker 
-        if urltg.find(".") > 0 and urltg.find("/") > 0 or urltg.startswith("www."):
-            if not urltg.startswith("https://") or not urltg.startswith("http://"):
-                urltg = "https://" + urltg
-        else:
+        tld_found = False
+        for tld in TLD_DOMAINS: 
+            tld = tld.casefold()
+            if urltg.casefold().find(f".{tld}") > 0:
+                if tld_found == False:
+                    if not urltg.startswith("https://") or not urltg.startswith("http://"):
+                        urltg = "https://" + urltg
+                tld_found = True
+        #else:
+        if tld_found == False:
             urltg = BROWSER_SE + urllib.parse.quote(urltg)
 
         self.browser.setUrl(QUrl(urltg))
