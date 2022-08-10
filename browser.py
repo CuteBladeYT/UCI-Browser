5b0cfa09ef4181e703c965af028609b323ea10da
UnitedCatdom <kgoszczycki3636@gmail.com>
2022-08-10 07:25:21 +0200

    Add comments

--- browser.py
+++ browser.py
@@ -1,15 +1,19 @@
 #!/bin/python3
+
+# libs, libs, libs
 import sys, os, json, urllib, platform
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import PyQt5.QtWebEngineWidgets as webengwid
 
+# settings
 _sf = open("settings.json")
 settings = json.loads(_sf.read())
 _sf.close()
 
 
+# check for icon
 iconext = "png"
 iswin = platform.win32_ver()[0] != ""
 ismac = platform.mac_ver()[0] != ""
@@ -19,12 +23,14 @@ if iswin: iconext = "ico"
 if ismac: iconext = "icns"
 
 
+# get homepage from settings
 HOMEPAGE_URL = settings.get("homepage")
 
-
+# and here search engine
 BROWSER_SE = settings.get("search_engine").get(settings.get("search_engine").get("current"))
 
 
+# and the actual app
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -41,18 +47,19 @@ class App(QMainWindow):
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setWindowIcon(QIcon(f"./icon/i.{iconext}"))
 
+        # if it's set to maximized in settings 
+        # then do so
         if settings.get("startup").get("maximized") == True:
             self.showMaximized()
 
 
-
-
+        # the actual browser
         self.browser = webengwid.QWebEngineView()
 
-        
-
+        # navbar no. 1
         self.navbar = QToolBar()
 
+        # navbar buttons
         browser_goback = QAction("<", self)
         browser_goback.triggered.connect(self.browser.back)
         self.navbar.addAction(browser_goback)
@@ -73,15 +80,18 @@ class App(QMainWindow):
         self.browser_urlbar.returnPressed.connect(lambda: self.go_to_url(None))
         self.navbar.addWidget(self.browser_urlbar)
 
+        # on url changed / on title changed
         self.browser.urlChanged.connect(lambda: self.set_urlbar_url(None))
         self.browser.titleChanged.connect(lambda: self.setWindowTitle(self.browser.title() + " - UCI Browser"))
 
 
 
 
-
+        # navbar no.2
+        # actually pagebar, for tabs
         self.pagebar = QToolBar() 
 
+        # button for opening new page
         newpagetab = QAction("+", self) 
         newpagetab.triggered.connect(lambda: self.new_page(HOMEPAGE_URL))
         self.pagebar.addAction(newpagetab)
@@ -100,21 +110,26 @@ class App(QMainWindow):
 
 
 
+    # the code could just use self.browser.setUrl(), but 
+    # to comfortable use it some more ricing is needed
     def go_to_url(self, url):
         urltg = url
         if url == None:
             urltg = self.browser_urlbar.text()
 
+        # smart but dumb url checker 
         if urltg.find(".") > 0 and urltg.find("/") > 0:
             if not urltg.startswith("https://" or "http://"):
                 urltg = "https://" + urltg
         else:
             urltg = BROWSER_SE + urllib.parse.quote(urltg)
 
-        print(urltg)
+        # print(urltg) # debug purposes
 
         self.browser.setUrl(QUrl(urltg))
 
+    # now when the url is changed sync it with 
+    # url bar
     def set_urlbar_url(self, url):
         urll = url
         if url == None:
@@ -126,6 +141,7 @@ class App(QMainWindow):
 
         history = ""
 
+        # ...and save the URL to the history
         if os.path.exists(f"{os.getcwd()}/history") == False:
             _f = open("history", "w+")
             _f.write("")
@@ -146,6 +162,7 @@ class App(QMainWindow):
 
 
 
+    # create new page
     def new_page(self, url):
         page = webengwid.QWebEnginePage() 
         if url == None:
@@ -167,6 +184,7 @@ class App(QMainWindow):
         self.pagebar.addAction(acitem)
         self.pagebar.addAction(closebtn)
 
+    # set its title
     def set_page_title(self, page, acitem):
         title = page.title()
         _title = title
@@ -180,6 +198,8 @@ class App(QMainWindow):
 
         acitem.setText(title)
 
+    # and close the page
+    # (yeah...)
     def close_page(self, acitem, closebtn):
         self.pagebar.removeAction(acitem)
         self.pagebar.removeAction(closebtn)
